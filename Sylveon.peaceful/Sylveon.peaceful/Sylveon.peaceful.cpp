c0942976f9984d6984c82fea18d84dfe96aeec0f
MrSuperBuddy <141313920+red-wipet@users.noreply.github.com>
2024-01-28 21:29:47 +0300

    Add files via upload

---
+++ Sylveon.peaceful/Sylveon.peaceful/Sylveon.peaceful.cpp
@@ -0,0 +1,689 @@
+﻿
+
+#include <windows.h>
+#include <tchar.h>
+#include <ctime>
+#include <iostream>
+#include <sstream>
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib,"Msimg32.lib")
+#include <math.h>
+#include <cmath>
+#include <time.h>
+typedef struct
+{
+	float x;
+	float y;
+	float z;
+} VERTEX;
+
+typedef struct
+{
+	int vtx0;
+	int vtx1;
+} EDGE;
+#define M_PI   3.14159265358979323846264338327950288
+//#define PI   3.14159265358979323846264338327950288
+typedef NTSTATUS(NTAPI* NRHEdef)(NTSTATUS, ULONG, ULONG, PULONG, ULONG, PULONG);
+typedef NTSTATUS(NTAPI* RAPdef)(ULONG, BOOLEAN, BOOLEAN, PBOOLEAN);
+typedef union _RGBQUAD {
+	COLORREF rgb;
+	struct {
+		BYTE b;
+		BYTE g;
+		BYTE r;
+		BYTE Reserved;
+	};
+}_RGBQUAD, * PRGBQUAD;
+typedef struct
+{
+	FLOAT h;
+	FLOAT s;
+	FLOAT l;
+} HSL;
+
+namespace Colors
+{
+	HSL rgb2hsl(RGBQUAD rgb)
+	{
+		HSL hsl;
+
+		BYTE r = rgb.rgbRed;
+		BYTE g = rgb.rgbGreen;
+		BYTE b = rgb.rgbBlue;
+
+		FLOAT _r = (FLOAT)r / 255.f;
+		FLOAT _g = (FLOAT)g / 255.f;
+		FLOAT _b = (FLOAT)b / 255.f;
+
+		FLOAT rgbMin = min(min(_r, _g), _b);
+		FLOAT rgbMax = max(max(_r, _g), _b);
+
+		FLOAT fDelta = rgbMax - rgbMin;
+		FLOAT deltaR;
+		FLOAT deltaG;
+		FLOAT deltaB;
+
+		FLOAT h = 0.f;
+		FLOAT s = 0.f;
+		FLOAT l = (FLOAT)((rgbMax + rgbMin) / 2.f);
+
+		if (fDelta != 0.f)
+		{
+			s = l < .5f ? (FLOAT)(fDelta / (rgbMax + rgbMin)) : (FLOAT)(fDelta / (2.f - rgbMax - rgbMin));
+			deltaR = (FLOAT)(((rgbMax - _r) / 6.f + (fDelta / 2.f)) / fDelta);
+			deltaG = (FLOAT)(((rgbMax - _g) / 6.f + (fDelta / 2.f)) / fDelta);
+			deltaB = (FLOAT)(((rgbMax - _b) / 6.f + (fDelta / 2.f)) / fDelta);
+
+			if (_r == rgbMax)      h = deltaB - deltaG;
+			else if (_g == rgbMax) h = (1.f / 3.f) + deltaR - deltaB;
+			else if (_b == rgbMax) h = (2.f / 3.f) + deltaG - deltaR;
+			if (h < 0.f)           h += 1.f;
+			if (h > 1.f)           h -= 1.f;
+		}
+
+		hsl.h = h;
+		hsl.s = s;
+		hsl.l = l;
+		return hsl;
+	}
+
+	RGBQUAD hsl2rgb(HSL hsl)
+	{
+		RGBQUAD rgb;
+
+		FLOAT r = hsl.l;
+		FLOAT g = hsl.l;
+		FLOAT b = hsl.l;
+
+		FLOAT h = hsl.h;
+		FLOAT sl = hsl.s;
+		FLOAT l = hsl.l;
+		FLOAT v = (l <= .5f) ? (l * (1.f + sl)) : (l + sl - l * sl);
+
+		FLOAT m;
+		FLOAT sv;
+		FLOAT fract;
+		FLOAT vsf;
+		FLOAT mid1;
+		FLOAT mid2;
+
+		INT sextant;
+
+		if (v > 0.f)
+		{
+			m = l + l - v;
+			sv = (v - m) / v;
+			h *= 6.f;
+			sextant = (INT)h;
+			fract = h - sextant;
+			vsf = v * sv * fract;
+			mid1 = m + vsf;
+			mid2 = v - vsf;
+
+			switch (sextant)
+			{
+			case 0:
+				r = v;
+				g = mid1;
+				b = m;
+				break;
+			case 1:
+				r = mid2;
+				g = v;
+				b = m;
+				break;
+			case 2:
+				r = m;
+				g = v;
+				b = mid1;
+				break;
+			case 3:
+				r = m;
+				g = mid2;
+				b = v;
+				break;
+			case 4:
+				r = mid1;
+				g = m;
+				b = v;
+				break;
+			case 5:
+				r = v;
+				g = m;
+				b = mid2;
+				break;
+			}
+		}
+
+		rgb.rgbRed = (BYTE)(r * 255.f);
+		rgb.rgbGreen = (BYTE)(g * 255.f);
+		rgb.rgbBlue = (BYTE)(b * 255.f);
+
+		return rgb;
+	}
+}
+int red, green, blue;
+bool ifcolorblue = false, ifblue = false;
+COLORREF Hue(int length) { //Credits to Void_/GetMBR
+	if (red != length) {
+		red < length; red++;
+		if (ifblue == true) {
+			return RGB(red, 0, length);
+		}
+		else {
+			return RGB(red, 0, 0);
+		}
+	}
+	else {
+		if (green != length) {
+			green < length; green++;
+			return RGB(length, green, 0);
+		}
+		else {
+			if (blue != length) {
+				blue < length; blue++;
+				return RGB(0, length, blue);
+			}
+			else {
+				red = 0; green = 0; blue = 0;
+				ifblue = true;
+			}
+		}
+	}
+}
+
+VOID WINAPI ci(int x, int y, int w, int h)
+{
+	HDC hdc = GetDC(0);
+	HBRUSH brush = CreateSolidBrush(RGB(255, 0, 0));
+	SelectObject(hdc, brush);
+	BitBlt(hdc, x, y, w, h, hdc, x, y, 0x123456);
+	DeleteObject(brush);
+	ReleaseDC(NULL, hdc);
+}
+
+VOID WINAPI ci1(int x, int y, int w, int h)
+{
+	HDC hdc = GetDC(0);
+	HBRUSH brush = CreateSolidBrush(RGB(0, 255, 0));
+	SelectObject(hdc, brush);
+	BitBlt(hdc, x, y, w, h, hdc, x, y, 0x123456);
+	DeleteObject(brush);
+	ReleaseDC(NULL, hdc);
+}
+
+VOID WINAPI ci2(int x, int y, int w, int h)
+{
+	HDC hdc = GetDC(0);
+	HBRUSH brush = CreateSolidBrush(RGB(0, 0, 255));
+	SelectObject(hdc, brush);
+	BitBlt(hdc, x, y, w, h, hdc, x, y, 0x123456);
+	DeleteObject(brush);
+	ReleaseDC(NULL, hdc);
+}
+DWORD WINAPI payload1(LPVOID lpParam)
+{
+	HDC desk = GetDC(0);
+	const int size = 1000;
+	int sw = GetSystemMetrics(0), sh = GetSystemMetrics(1);
+	int i = 0; i < size; i += 100;
+	while (1) {
+
+
+		int radius = 10, rx = rand() % sw, ry = rand() % sh, num = rand() % 5;
+
+		for (int t = 0; t < sw + sh; ++t) {
+			int x = (int)(float)(radius + t * tan(t + radius * 3) + rx),
+
+				y = (int)(float)(radius + t * cos(t + radius * 3) + ry);
+
+			if (rand() % 5 == 0) { ci(x, y, i, i); Sleep(0.99); }
+
+			if (rand() % 5 == 0) { ci1(x - i / 1, y - i / 1, i, i); Sleep(0.99); }
+
+			if (rand() % 5 == 0) { ci2(x - i / 1, y - i / 1, i, i); Sleep(0.99); }
+
+
+		}
+
+		InvalidateRect(0, 0, 0);
+
+	}
+
+}
+DWORD WINAPI payload2(LPVOID lpParam) {
+	HDC desk = GetDC(0);
+
+	int sw = GetSystemMetrics(0), sh = GetSystemMetrics(1);
+
+	while (1) {
+
+
+		int radius = 10, rx = rand() % sw, ry = rand() % sh, num = rand() % 5;
+
+		for (int t = 0; t < sw + sh; ++t) {
+			int x = (int)(float)(radius + t * sin(t - radius * 100) + rx),
+
+				y = (int)(float)(radius + t * sin(t - radius * 100) + ry);
+
+			if (rand() % 5 == 0) { DrawIcon(desk, x, y, LoadIcon(0, IDI_ERROR)); Sleep(0.99); }
+
+			if (rand() % 5 == 0) { DrawIcon(desk, x, y, LoadIcon(0, IDI_WARNING)); Sleep(0.99); }
+
+			if (rand() % 5 == 0) { DrawIcon(desk, x, y, LoadIcon(0, IDI_INFORMATION)); Sleep(0.99); }
+
+			if (rand() % 5 == 0) { DrawIcon(desk, x, y, LoadIcon(0, IDI_INFORMATION)); Sleep(0.99); }
+
+			if (rand() % 5 == 0) { DrawIcon(desk, x, y, LoadIcon(0, IDI_ASTERISK)); Sleep(0.99); }
+
+		}
+
+		InvalidateRect(0, 0, 0);
+
+	}
+
+}
+DWORD WINAPI payload3(LPVOID lpParam) {
+	HDC hdc = GetDC(NULL);
+	HDC hdcCopy = CreateCompatibleDC(hdc);
+	int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+	int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+	BITMAPINFO bmpi = { 0 };
+	HBITMAP bmp;
+
+	bmpi.bmiHeader.biSize = sizeof(bmpi);
+	bmpi.bmiHeader.biWidth = screenWidth;
+	bmpi.bmiHeader.biHeight = screenHeight;
+	bmpi.bmiHeader.biPlanes = 1;
+	bmpi.bmiHeader.biBitCount = 32;
+	bmpi.bmiHeader.biCompression = BI_RGB;
+
+
+	RGBQUAD* rgbquad = NULL;
+	HSL hslcolor;
+
+	bmp = CreateDIBSection(hdc, &bmpi, DIB_RGB_COLORS, (void**)&rgbquad, NULL, 0);
+	SelectObject(hdcCopy, bmp);
+
+	INT i = 0;
+
+	while (1)
+	{
+		hdc = GetDC(NULL);
+		StretchBlt(hdcCopy, 0, 0, screenWidth, screenHeight, hdc, 0, 0, screenWidth, screenHeight, SRCCOPY);
+
+		RGBQUAD rgbquadCopy;
+
+		for (int x = 0; x < screenWidth; x++)
+		{
+			for (int y = 0; y < screenHeight; y++)
+			{
+				int index = y * screenWidth + x;
+
+				int fx = (int)((i ^ 4) + (i * 4) * cbrt(x ^ i ^ i ^ y + x ^ i ^ y + i * x + y));
+
+				rgbquadCopy = rgbquad[index];
+
+				hslcolor = Colors::rgb2hsl(rgbquadCopy);
+				hslcolor.h = fmod(fx / 100.f + y / screenHeight * .2f, 1.f);
+				hslcolor.s = 0.700f;
+
+				rgbquad[index] = Colors::hsl2rgb(hslcolor);
+			}
+		}
+
+		i++;
+
+		StretchBlt(hdc, 0, 0, screenWidth, screenHeight, hdcCopy, 0, 0, screenWidth, screenHeight, SRCCOPY);
+		ReleaseDC(NULL, hdc);
+		DeleteDC(hdc);
+	}
+
+	return 0x00;
+}
+DWORD WINAPI payload4(LPVOID lpParam) {
+	HDC hdc = GetDC(NULL);
+	HDC hdcCopy = CreateCompatibleDC(hdc);
+	int w = GetSystemMetrics(0);
+	int h = GetSystemMetrics(1);
+	BITMAPINFO bmpi = { 0 };
+	HBITMAP bmp;
+
+	bmpi.bmiHeader.biSize = sizeof(bmpi);
+	bmpi.bmiHeader.biWidth = w;
+	bmpi.bmiHeader.biHeight = h;
+	bmpi.bmiHeader.biPlanes = 1;
+	bmpi.bmiHeader.biBitCount = 32;
+	bmpi.bmiHeader.biCompression = BI_RGB;
+
+	RGBQUAD* rgbquad = NULL;
+	HSL hslcolor;
+
+	bmp = CreateDIBSection(hdc, &bmpi, DIB_RGB_COLORS, (void**)&rgbquad, NULL, 0);
+	SelectObject(hdcCopy, bmp);
+
+	INT i = 0;
+
+	while (1)
+	{
+		hdc = GetDC(NULL);
+		StretchBlt(hdcCopy, 0, 0, w, h, hdc, 0, 0, w, h, SRCCOPY);
+
+		RGBQUAD rgbquadCopy;
+
+		for (int x = 0; x < w; x++)
+		{
+			for (int y = 0; y < h; y++)
+			{
+				int index = y * w + x;
+
+				FLOAT fx = ((x + (i * 100)) + (y ^ (i & 4))) ^ ((x >> 2) + (y << 1));
+
+				rgbquadCopy = rgbquad[index];
+
+				hslcolor = Colors::rgb2hsl(rgbquadCopy);
+				hslcolor.h = fmod(fx / 300.f + y / h * .1f, 1.f);
+				hslcolor.s = 0.700f;
+				rgbquad[index] = Colors::hsl2rgb(hslcolor);
+			}
+		}
+
+		i++;
+		StretchBlt(hdc, 0, 0, w, h, hdcCopy, 0, 0, w, h, SRCCOPY);
+		ReleaseDC(NULL, hdc); DeleteDC(hdc);
+	}
+
+	return 0x00;
+}
+DWORD WINAPI payload5(LPVOID lpParam) {
+	HDC hdc = GetDC(NULL);
+	HDC hdcCopy = CreateCompatibleDC(hdc);
+	int w = GetSystemMetrics(0);
+	int h = GetSystemMetrics(1);
+	BITMAPINFO bmpi = { 0 };
+	HBITMAP bmp;
+
+	bmpi.bmiHeader.biSize = sizeof(bmpi);
+	bmpi.bmiHeader.biWidth = w;
+	bmpi.bmiHeader.biHeight = h;
+	bmpi.bmiHeader.biPlanes = 1;
+	bmpi.bmiHeader.biBitCount = 32;
+	bmpi.bmiHeader.biCompression = BI_RGB;
+
+	RGBQUAD* rgbquad = NULL;
+	HSL hslcolor;
+
+	bmp = CreateDIBSection(hdc, &bmpi, DIB_RGB_COLORS, (void**)&rgbquad, NULL, 0);
+	SelectObject(hdcCopy, bmp);
+
+	INT i = 0;
+
+	while (1)
+	{
+		hdc = GetDC(NULL);
+		StretchBlt(hdcCopy, 0, 0, w, h, hdc, 0, 0, w, h, SRCCOPY);
+
+		RGBQUAD rgbquadCopy;
+
+		for (int x = 0; x < w; x++)
+		{
+			for (int y = 0; y < h; y++)
+			{
+				int index = y * w + x;
+
+				FLOAT fx = ((x + (i * 100)) + (y ^ (i & 4))) ^ ((x >> 1) + (y << 1));
+
+				rgbquadCopy = rgbquad[index];
+
+				hslcolor = Colors::rgb2hsl(rgbquadCopy);
+				hslcolor.h = fmod(fx / 300.f + y / h * .1f, 1.f);
+				hslcolor.s = 0.700f;
+				rgbquad[index] = Colors::hsl2rgb(hslcolor);
+			}
+		}
+
+		i++;
+		StretchBlt(hdc, 0, 0, w, h, hdcCopy, 0, 0, w, h, SRCCOPY);
+		ReleaseDC(NULL, hdc); DeleteDC(hdc);
+	}
+
+	return 0x00;
+}
+DWORD WINAPI payload6(LPVOID lpParam) {
+	HDC hdc = GetDC(NULL);
+	HDC hdcCopy = CreateCompatibleDC(hdc);
+	int w = GetSystemMetrics(0);
+	int h = GetSystemMetrics(1);
+	BITMAPINFO bmpi = { 0 };
+	HBITMAP bmp;
+
+	bmpi.bmiHeader.biSize = sizeof(bmpi);
+	bmpi.bmiHeader.biWidth = w;
+	bmpi.bmiHeader.biHeight = h;
+	bmpi.bmiHeader.biPlanes = 1;
+	bmpi.bmiHeader.biBitCount = 32;
+	bmpi.bmiHeader.biCompression = BI_RGB;
+
+	RGBQUAD* rgbquad = NULL;
+	HSL hslcolor;
+
+	bmp = CreateDIBSection(hdc, &bmpi, DIB_RGB_COLORS, (void**)&rgbquad, NULL, 0);
+	SelectObject(hdcCopy, bmp);
+
+	INT i = 0;
+
+	while (1)
+	{
+		hdc = GetDC(NULL);
+		StretchBlt(hdcCopy, 0, 0, w, h, hdc, 0, 0, w, h, SRCCOPY);
+
+		RGBQUAD rgbquadCopy;
+
+		for (int x = 0; x < w; x++)
+		{
+			for (int y = 0; y < h; y++)
+			{
+				int index = y * w + x;
+
+				FLOAT fx = ((x + (i * 100)) + (y ^ (i & 4))) ^ ((x >> 1) + (y >> 200));
+
+				rgbquadCopy = rgbquad[index];
+
+				hslcolor = Colors::rgb2hsl(rgbquadCopy);
+				hslcolor.h = fmod(fx / 300.f + y / h * .1f, 1.f);
+				hslcolor.s = 0.700f;
+				rgbquad[index] = Colors::hsl2rgb(hslcolor);
+			}
+		}
+
+		i++;
+		StretchBlt(hdc, 0, 0, w, h, hdcCopy, 0, 0, w, h, SRCCOPY);
+		ReleaseDC(NULL, hdc); DeleteDC(hdc);
+	}
+
+	return 0x00;
+}
+DWORD WINAPI payload7(LPVOID lpParam) {
+	HDC hdc = GetDC(NULL);
+	HDC hdcCopy = CreateCompatibleDC(hdc);
+	int w = GetSystemMetrics(0);
+	int h = GetSystemMetrics(1);
+	BITMAPINFO bmpi = { 0 };
+	HBITMAP bmp;
+
+	bmpi.bmiHeader.biSize = sizeof(bmpi);
+	bmpi.bmiHeader.biWidth = w;
+	bmpi.bmiHeader.biHeight = h;
+	bmpi.bmiHeader.biPlanes = 1;
+	bmpi.bmiHeader.biBitCount = 32;
+	bmpi.bmiHeader.biCompression = BI_RGB;
+
+	RGBQUAD* rgbquad = NULL;
+	HSL hslcolor;
+
+	bmp = CreateDIBSection(hdc, &bmpi, DIB_RGB_COLORS, (void**)&rgbquad, NULL, 0);
+	SelectObject(hdcCopy, bmp);
+
+	INT i = 0;
+
+	while (1)
+	{
+		hdc = GetDC(NULL);
+		StretchBlt(hdcCopy, 0, 0, w, h, hdc, 0, 0, w, h, SRCCOPY);
+
+		RGBQUAD rgbquadCopy;
+
+		for (int x = 0; x < w; x++)
+		{
+			for (int y = 0; y < h; y++)
+			{
+				int index = y * w + x;
+
+				FLOAT fx = ((x + (i * 100)) + (y ^ (i & 4))) ^ ((x >> 2) + (y >> 1));
+
+				rgbquadCopy = rgbquad[index];
+
+				hslcolor = Colors::rgb2hsl(rgbquadCopy);
+				hslcolor.h = fmod(fx / 300.f + y / h * .1f, 1.f);
+				hslcolor.s = 0.700f;
+				rgbquad[index] = Colors::hsl2rgb(hslcolor);
+			}
+		}
+
+		i++;
+		StretchBlt(hdc, 0, 0, w, h, hdcCopy, 0, 0, w, h, SRCCOPY);
+		ReleaseDC(NULL, hdc); DeleteDC(hdc);
+	}
+
+	return 0x00;
+}
+DWORD WINAPI payload8(LPVOID lpParam) {
+	HDC desk = GetDC(0);
+
+	int sw = GetSystemMetrics(0), sh = GetSystemMetrics(1);
+
+	while (1) {
+
+		desk = GetDC(0);
+
+
+
+		StretchBlt(desk, 0, 0, sw + 0, sh + 0, desk, sw, sh, -sw, -sh, SRCCOPY);
+
+		Sleep(150);
+
+	}
+
+}
+DWORD WINAPI payload9(LPVOID lpParam) {
+	HDC hdc = GetDC(NULL);
+	HDC hdcCopy = CreateCompatibleDC(hdc);
+	int w = GetSystemMetrics(0);
+	int h = GetSystemMetrics(1);
+	BITMAPINFO bmpi = { 0 };
+	HBITMAP bmp;
+
+	bmpi.bmiHeader.biSize = sizeof(bmpi);
+	bmpi.bmiHeader.biWidth = w;
+	bmpi.bmiHeader.biHeight = h;
+	bmpi.bmiHeader.biPlanes = 1;
+	bmpi.bmiHeader.biBitCount = 32;
+	bmpi.bmiHeader.biCompression = BI_RGB;
+
+	RGBQUAD* rgbquad = NULL;
+	HSL hslcolor;
+
+	bmp = CreateDIBSection(hdc, &bmpi, DIB_RGB_COLORS, (void**)&rgbquad, NULL, 0);
+	SelectObject(hdcCopy, bmp);
+
+	INT i = 0;
+
+	while (1)
+	{
+		hdc = GetDC(NULL);
+		StretchBlt(hdcCopy, 0, 0, w, h, hdc, 0, 0, w, h, SRCCOPY);
+
+		RGBQUAD rgbquadCopy;
+
+		for (int x = 0; x < w; x++)
+		{
+			for (int y = 0; y < h; y++)
+			{
+				int index = y * w + x;
+
+				FLOAT fx = ((x + (i * 100)) + (y ^ (i & 4))) ^ ((x >> 2) - (y >> 1));
+
+				rgbquadCopy = rgbquad[index];
+
+				hslcolor = Colors::rgb2hsl(rgbquadCopy);
+				hslcolor.h = fmod(fx / 300.f + y / h * .1f, 1.f);
+				hslcolor.s = 0.700f;
+				rgbquad[index] = Colors::hsl2rgb(hslcolor);
+			}
+		}
+
+		i++;
+		StretchBlt(hdc, 0, 0, w, h, hdcCopy, 0, 0, w, h, SRCCOPY);
+		ReleaseDC(NULL, hdc); DeleteDC(hdc);
+	}
+
+	return 0x00;
+}
+int main() {
+	ShowWindow(GetConsoleWindow(), SW_HIDE);
+	if (MessageBoxW(NULL, L"WARNING!!!\r\n\It doesn't delete all your data\r\n\Be fine ;)", L"Sylveon", MB_YESNO | MB_ICONEXCLAMATION) == IDNO)
+	{
+		ExitProcess(0);
+	}
+	else
+	{
+		if (MessageBoxW(NULL, L"F̷i̷n̸a̴l̴ ̴W̸a̴r̸n̷i̸n̷g̷ !!!\r\n\NOT FOR EPILEPSY!!!", L"F̷i̷n̸a̴l̴ ̴W̸a̴r̸n̷i̸n̷g̷  - Sylveon", MB_YESNO | MB_ICONEXCLAMATION) == IDNO)
+		{
+			ExitProcess(0);
+		}
+		else
+		{
+			HANDLE thread1 = CreateThread(0, 0, payload1, 0, 0, 0);
+			Sleep(30000);
+			TerminateThread(thread1, 0);
+			CloseHandle(thread1);
+			InvalidateRect(0, 0, 0);
+			HANDLE thread2 = CreateThread(0, 0, payload2, 0, 0, 0);
+			Sleep(30000);
+			TerminateThread(thread2, 0);
+			CloseHandle(thread2);
+			InvalidateRect(0, 0, 0);
+			HANDLE thread3 = CreateThread(0, 0, payload3, 0, 0, 0);
+			Sleep(30000);
+			TerminateThread(thread3, 0);
+			CloseHandle(thread3);
+			InvalidateRect(0, 0, 0);
+			HANDLE thread4 = CreateThread(0, 0, payload4, 0, 0, 0);
+			Sleep(30000);
+			TerminateThread(thread4, 0);
+			CloseHandle(thread4);
+			InvalidateRect(0, 0, 0);
+			HANDLE thread5 = CreateThread(0, 0, payload5, 0, 0, 0);
+			Sleep(30000);
+			TerminateThread(thread5, 0);
+			CloseHandle(thread5);
+			InvalidateRect(0, 0, 0);
+			HANDLE thread6 = CreateThread(0, 0, payload6, 0, 0, 0);
+			Sleep(30000);
+			TerminateThread(thread6, 0);
+			CloseHandle(thread6);
+			InvalidateRect(0, 0, 0);
+			HANDLE thread7 = CreateThread(0, 0, payload7, 0, 0, 0);
+			Sleep(30000);
+			TerminateThread(thread7, 0);
+			CloseHandle(thread7);
+			InvalidateRect(0, 0, 0);
+			HANDLE thread8 = CreateThread(0, 0, payload9, 0, 0, 0);
+			Sleep(30000);
+			TerminateThread(thread8, 0);
+			CloseHandle(thread8);
+			InvalidateRect(0, 0, 0);
+		}
+	}
+}
